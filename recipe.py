e3bea7464d923ed52290a9abbe16215a5f7b35a4
Arjun R Warrier <notwarrier@gmail.com>
2022-11-01 21:43:17 +0530

    Added database connection, added data entry option

--- recipe.py
+++ recipe.py
@@ -1,3 +1,10 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host = 'localhost', user = 'root', password = '', database = 'recipedb')
+
+mycursor = mydb.cursor()
+
+
 while True:
     print("\nSelect an option from the menu ")
     print("1. Add Recipe")
@@ -10,6 +17,19 @@ while True:
     choice = int(input("Enter an option: "))
     if(choice == 1):
         print("Recipe data insertion")
+
+        rtitle = input("Enter recipe name: ")
+        rdesc = input("Enter recipe description: ")
+        rpreparedby = input("Recipe prepared by: ")
+        ringredients = input("Enter the ingredients: ")
+        rcategory = input("Enter the recipe category: ")
+
+        sql = 'INSERT INTO `recipes`(`title`, `description`, `preparedby`, `ingredients`, `recipecategory`) VALUES(%s,%s,%s,%s,%s)'
+        data = (rtitle,rdesc,rpreparedby,ringredients,rcategory)
+        mycursor.execute(sql,data)
+        mydb.commit()
+        print("Recipe's data inserted successfully.")
+
     elif(choice == 2):
         print("View Recipe")
     elif(choice == 3):
